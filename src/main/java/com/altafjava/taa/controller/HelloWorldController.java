12be9ff33448efca5a5403e44c006c1fc301c858
Altaf <altaf@groupbima.com>
2019-11-11 18:45:41 +0530

    HelloWorldController

---
+++ src/main/java/com/altafjava/taa/controller/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.altafjava.taa.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.altafjava.taa.dto.HelloWorldBean;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Welcome to Spring Boot API";
+	}
+
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
+
+	@GetMapping("/hello-world-path-variable/{param}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String param) {
+		return new HelloWorldBean("Hello World Path Variable Output=" + param);
+	}
+
+	@GetMapping("/err")
+	public String error() {
+		throw new RuntimeException("Something went wrong");
+	}
+}
---
+++ src/main/java/com/altafjava/taa/dto/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.altafjava.taa.dto;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean() {
+		super();
+	}
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}

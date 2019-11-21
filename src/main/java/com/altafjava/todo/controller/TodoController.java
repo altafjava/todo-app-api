4b322df89df30cd0d5a489cd2816e76d036fdc2f
Altaf <altaf@groupbima.com>
2019-11-21 18:35:01 +0530

    delete todo api created

--- src/main/java/com/altafjava/todo/controller/TodoController.java
+++ src/main/java/com/altafjava/todo/controller/TodoController.java
@@ -2,7 +2,9 @@ package com.altafjava.todo.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,13 @@ public class TodoController {
 	@GetMapping
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable long id) {
+		Todo todo = todoService.deleteById(id);
+		if (todo != null)
+			return ResponseEntity.noContent().build();
+		return ResponseEntity.notFound().build();
 	}
 }
--- src/main/java/com/altafjava/todo/service/TodoService.java
+++ src/main/java/com/altafjava/todo/service/TodoService.java
@@ -9,12 +9,29 @@ import com.altafjava.todo.dto.Todo;
 @Service
 public class TodoService {
 
-	public List<Todo> findAll() {
-		List<Todo> todos = new ArrayList<>();
-		long id = 0;
+	private static List<Todo> todos = new ArrayList<>();
+	static long id = 0;
+	static {
 		todos.add(new Todo(++id, "Sam Altaf", "Learning Angular", new Date(), true));
 		todos.add(new Todo(++id, "Ahmad Shah", "Playing Cricket", new Date(), false));
 		todos.add(new Todo(++id, "Ankit Singh", "Teaching Java to their students", new Date(), false));
+	}
+
+	public List<Todo> findAll() {
 		return todos;
+	}
+
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		if (todo == null)
+			return null;
+		if (todos.remove(todo))
+			return todo;
+		else
+			return null;
+	}
+
+	private Todo findById(long id) {
+		return todos.stream().filter(todo -> todo.getId() == id).findAny().orElse(null);
 	}
 }
